6ddc217b311331656d6b3f08ab1ed590be113537
Maksim Pecherskiy <max@maksimize.com>
2015-08-05 15:10:11 -0700

    Add tagline in conf.

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -3,8 +3,75 @@ var global = {
 };
 
 var viewController = {
+  init: function() {
+    var vc = this;
+    cartodb.createVis('map', 'https://maksim2.cartodb.com/api/v2/viz/1387c31c-e546-11e4-a74b-0e853d047bba/viz.json', {
+      tiles_loader: true,
+      center_lat: 32.7150,
+      center_lon: -117.1625,
+      zoom: 11
+    })
+    .done(function(vis, layers) {
+      $('body').removeClass('.map-loading').addClass('map-loaded');
+      global.vis = vis;
+      global.layers = layers;
+      vc.initSubLayerWatch();
+      vc.initBottomBar();
+      // Default
+      vc.initIntro();
+      // Force intro
+      //initIntro(true);
+     })
+    .error(function(err) {
+      console.log(err);
+    });
+  },
+  initSubLayerWatch: function() {
+      viewController.clearState();
+
+      var $workLayers = $('#layer-selector a.layer-opt');
+
+      $workLayers.click(function(e) {
+        // Load Map Info
+        var target = $(e.target);
+        viewController.loadMapInfo(target);
+        viewController.executeOps(target);
+      
+        $('.sidebar-toggle').click();
+        
+        $workLayers.removeClass('active');
+        $('a', target).addClass('active');
+      });
+  },
+  initBottomBar: function () {
+    $('#bottom-bar .tab').click(viewController.bottomBarToggle);
+  },
+  initIntro: function(force) {
+      var force = force || false;
+      var cookie = this.getCookie("sdInfraIntro");
+      if (cookie == null || cookie == "" || force == true) {
+        this.setCookie("sdInfraIntro", "1",90);
+        startIntro();
+      }
+  },
+  getCookie: function(cname) {
+      var name = cname + "=";
+      var ca = document.cookie.split(';');
+      for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+      }
+      return "";
+  },
+  setCookie: function(cname, cvalue, exdays) {
+      var d = new Date();
+      d.setTime(d.getTime() + (exdays*24*60*60*1000));
+      var expires = "expires="+d.toUTCString();
+      document.cookie = cname + "=" + cvalue + "; " + expires;
+  },
   clearState: function() {
-    $('#helper_box .helper-box-section').text("");
+    $('#helper_box .helper_section').hide();
     var num_sublayers = global.layers[1].getSubLayerCount();
     for (var i = 1; i < num_sublayers; i++)
       global.layers[1].getSubLayer(i).hide();
@@ -26,6 +93,7 @@ var viewController = {
     var subLayer = global.layers[1].getSubLayer(subLayerNum);
     subLayer.setSQL(sqlBuilder.getLayerSQL(subLayerID));
     subLayer.show();
+    $('#helper_box #' +  subLayerID + ".helper_section").show();
   },
   executeOps: function(target) {
     // Check for Ops, Execute as Needed:
@@ -61,114 +129,11 @@ var viewController = {
 
 
 
-function applyTemplates() {
-  var layerOptions = window.layerOptions;
-  var linkTemplate = _.template($( "script.sidebarLink" ).html());
-  var helperBoxTemplate = _.template($( "script.helperBox" ).html());
-  _.each(layerOptions, function(element, index) {
-    var templateVars = _.assign(element, { key: index });
-    var tLink = linkTemplate(templateVars);
-    var hLink = helperBoxTemplate(templateVars);
-    $('ul#work-layers').append(tLink);
-    $('#helper_box #helper_box_top').append(hLink);
-  });
-}
 
 
-function initSubLayerWatch() {
-  viewController.clearState();
 
-  var $workLayers = $('#layer-selector a.layer-opt');
-
-  $workLayers.click(function(e) {
-    // Load Map Info
-    var target = $(e.target);
-    viewController.loadMapInfo(target);
-    viewController.executeOps(target);
-    
-
-    $('.sidebar-toggle').click();
-    
-  
-
-    
-    //$('#helper_box').show();
-    //$('#helper_box #' + subLayerID).show();
-
-    // Deselect all and select the clicked one
-    $workLayers.removeClass('active');
-    $('a', target).addClass('active');
-  });
-}
-
-function initBottomBar() {
-  $('#bottom-bar .tab').click(viewController.bottomBarToggle);
-}
-
-
-function initIntro(force) {
-  var force = force || false;
-  var intro = introJs().setOptions(window.introOptions);
-	var cookie=getCookie("sdInfraIntro");
-  if (cookie==null || cookie=="" || force == true) {
-    setCookie("sdInfraIntro", "1",90);
-    intro.start();
-  }
-}
-
-function initMarkdown() {
-  $('.modal-body').each(function(index, element) {
-    var md = $(element).text();
-    console.log(md);
-    $(element).html(marked(md));
-  })
-}
-
-function getCookie(cname) {
-  var name = cname + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0; i<ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0)==' ') c = c.substring(1);
-    if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
-  }
-  return "";
-}
-
-function setCookie(cname, cvalue, exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  var expires = "expires="+d.toUTCString();
-  document.cookie = cname + "=" + cvalue + "; " + expires;
-}
-
- function main() {
-  cartodb.createVis('map', 'https://maksim2.cartodb.com/api/v2/viz/1387c31c-e546-11e4-a74b-0e853d047bba/viz.json', {
-    tiles_loader: true,
-    center_lat: 32.7150,
-    center_lon: -117.1625,
-    zoom: 11
-  })
-  .done(function(vis, layers) {
-    $('body').removeClass('.map-loading').addClass('map-loaded');
-    global.vis = vis;
-    global.layers = layers;
-    /*_.templateSettings.variable = "rc";*/
-    /*applyTemplates();*/
-    initSubLayerWatch();
-    /*initMarkdown();*/
-    initBottomBar();
-    // Default
-    initIntro();
-    // Force intro
-    //initIntro(true);
-   })
-  .error(function(err) {
-    console.log(err);
-  });
-}
 
 $(document).ready(function() {
-  main();
+  viewController.init();
 });
 
