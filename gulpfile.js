58a932bfbfd167f5304df08e710b61631af76886
Maksim Pecherskiy <max@maksimize.com>
2015-08-20 13:09:16 -0700

    Prep CI.

--- gulpfile.js
+++ gulpfile.js
@@ -112,7 +112,7 @@ gulp.task("html", ["styles"], function () {
 
 
 // Task to upload your site to your personal GH Pages repo
-gulp.task("deploy", ["publish"], function () {
+gulp.task("deploy", function () {
   // Deploys your optimized site, you can change the settings in the html task if you want to
   return gulp.src("./site/**/*")
     .pipe($.ghPages({
