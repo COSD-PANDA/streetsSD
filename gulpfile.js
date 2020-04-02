615dbeb2a6fa490019a0c7cc89517f5d983411ed
Andrell Bower <bowera@gmail.com>
2020-04-01 21:03:51 -0700

    Revert "Overhaul to Hugo and ArcGIS online"

---
+++ gulpfile.js
@@ -0,0 +1,175 @@
+// Generated on 2015-08-04 using generator-jekyllized 0.7.3
+"use strict";
+
+var gulp = require("gulp");
+// Loads the plugins without having to list all of them, but you need
+// to call them as $.pluginname
+var $ = require("gulp-load-plugins")();
+// "del" is used to clean out directories and such
+var del = require("del");
+// BrowserSync isn"t a gulp package, and needs to be loaded manually
+var browserSync = require("browser-sync");
+// merge is used to merge the output from two different streams into the same stream
+var merge = require("merge-stream");
+// Need a command for reloading webpages using BrowserSync
+var reload = browserSync.reload;
+// And define a variable that BrowserSync uses in it"s function
+var bs;
+var path = require('path')
+
+// Deletes the directory that is used to serve the site during development
+gulp.task("clean:dev", del.bind(null, ["serve"]));
+
+// Deletes the directory that the optimized site is output to
+gulp.task("clean:prod", del.bind(null, ["site"]));
+
+// Runs the build command for Jekyll to compile the site locally
+// This will build the site with the production settings
+gulp.task("jekyll:dev", $.shell.task("bundle exec jekyll build"));
+gulp.task("jekyll-rebuild", ["jekyll:dev"], function () {
+  reload;
+});
+
+// Almost identical to the above task, but instead we load in the build configuration
+// that overwrites some of the settings in the regular configuration so that you
+// don"t end up publishing your drafts or future posts
+gulp.task("jekyll:prod", $.shell.task("bundle exec jekyll build --config _config.yml,_config.build.yml"));
+
+// Compiles the SASS files and moves them into the "assets/stylesheets" directory
+gulp.task("styles", function () {
+  // Looks at the style.scss file for what to include and creates a style.css file
+  return gulp.src("src/assets/scss/style.scss")
+    .pipe($.sass())
+    // AutoPrefix your CSS so it works between browsers
+    .pipe($.autoprefixer("last 1 version", { cascade: true }))
+    // Directory your CSS file goes to
+    .pipe(gulp.dest("src/assets/stylesheets/"))
+    .pipe(gulp.dest("serve/assets/stylesheets/"))
+    // Outputs the size of the CSS file
+    .pipe($.size({title: "styles"}))
+    // Injects the CSS changes to your browser since Jekyll doesn"t rebuild the CSS
+    .pipe(reload({stream: true}));
+});
+
+// Optimizes the images that exists
+gulp.task("images", function () {
+  return gulp.src("src/assets/images/**")
+    .pipe($.changed("site/assets/images"))
+    .pipe($.imagemin({
+      // Lossless conversion to progressive JPGs
+      progressive: true,
+      // Interlace GIFs for progressive rendering
+      interlaced: true
+    }))
+    .pipe(gulp.dest("site/assets/images"))
+    .pipe($.size({title: "images"}));
+});
+
+// Copy over fonts to the "site" directory
+gulp.task("fonts", function () {
+  return gulp.src("src/assets/fonts/**")
+    .pipe(gulp.dest("site/assets/fonts"))
+    .pipe($.size({ title: "fonts" }));
+});
+
+// Copy xml and txt files to the "site" directory
+gulp.task("copy", function () {
+  return gulp.src(["serve/*.txt", "serve/*.xml", "src/CNAME", 'serve/*.json'])
+    .pipe(gulp.dest("site"))
+    .pipe($.size({ title: "xml, txt, json" }))
+});
+
+// Optimizes all the CSS, HTML and concats the JS etc
+
+
+gulp.task("html", ["styles"], function () {
+  var jsFilter = $.filter("**/*.js", { restore: true });
+  var cssFilter = $.filter("**/*.css", { restore: true });
+  var htmlFilter = $.filter(['**/*', '!**/*.html'], { restore: true });
+
+  return gulp.src(["serve/**/*.html", '!serve/assets/bower_components/**/*'])
+    // Concatenate Files
+    .pipe($.useref({searchPath: "serve" }))
+    // Only for JS, Uglify.
+    //.pipe(jsFilter)
+    //.pipe($.uglify({preserveComments: "some"}))
+    //.pipe(jsFilter.restore)
+    // Only for CSS - Minify
+    .pipe(cssFilter)
+    .pipe($.cleanCss())
+    .pipe(cssFilter.restore)
+    // Only for NON-HTML, revision file names
+    .pipe(htmlFilter)
+    .pipe($.revAll({ ignore: [".eot", ".svg", ".ttf", ".woff", ".csv", ".json", ".jpg", ".png"] }))
+    .pipe(htmlFilter.restore)
+    // Sub in new file names
+    .pipe($.revReplace())
+    // Minify HTML
+    /*.pipe($.htmlmin({
+      removeComments: true,
+      removeCommentsFromCDATA: true,
+      removeCDATASectionsFromCDATA: true,
+      collapseWhitespace: true,
+      collapseBooleanAttributes: true,
+      removeAttributeQuotes: true,
+      removeRedundantAttributes: true
+    }))*/
+    .pipe(gulp.dest("site"))
+    .pipe($.size({title: "optimizations"}));
+});
+
+
+// Runs "jekyll doctor" on your site to check for errors with your configuration
+// and will check for URL errors a well
+gulp.task("doctor", $.shell.task("bundle exec jekyll doctor"));
+
+// BrowserSync will serve our site on a local server for us and other devices to use
+// It will also autoreload across all devices as well as keep the viewport synchronized
+// between them.
+gulp.task("serve:dev", ["styles", "jekyll:dev"], function () {
+  bs = browserSync({
+    notify: true,
+    // tunnel: "",
+    server: {
+      baseDir: "serve"
+    },
+    host: process.env.IP || 'localhost',
+    port: process.env.PORT || '3000'
+  });
+});
+
+// These tasks will look for files that change while serving and will auto-regenerate or
+// reload the website accordingly. Update or add other files you need to be watched.
+gulp.task("watch", function () {
+  gulp.watch(["src/**/*.md", "src/**/*.html", "src/**/*.xml", "src/**/*.txt", "src/**/*.js"], ["jekyll-rebuild"]);
+  gulp.watch(["serve/assets/stylesheets/*.css"], reload);
+  gulp.watch(["src/assets/scss/**/*.scss"], ["styles"]);
+});
+
+// Serve the site after optimizations to see that everything looks fine
+gulp.task("serve:prod", function () {
+  bs = browserSync({
+    notify: false,
+    // tunnel: true,
+    server: {
+      baseDir: "site"
+    }
+  });
+});
+
+// Default task, run when just writing "gulp" in the terminal
+gulp.task("default", ["serve:dev", "watch"]);
+
+// Checks your CSS, JS and Jekyll for errors
+gulp.task("check", ["doctor"], function () {
+  // Better hope nothing is wrong.
+});
+
+// Builds the site but doesn"t serve it to you
+gulp.task("build", ["jekyll:prod", "styles"], function () {});
+
+// Builds your site with the "build" command and then runs all the optimizations on
+// it and outputs it to "./site"
+gulp.task("publish", ["build"], function () {
+  gulp.start("html", "copy", "images", "fonts");
+});
---
+++ src/assets/javascript/opsControl.js
@@ -0,0 +1,103 @@
+var opsControl = (function() {
+  // Private.
+
+  sql = new cartodb.SQL({
+    user: 'cityofsandiego-admin',
+    sql_api_template: 'https://{user}.carto.com',
+  });
+
+  tDistanceStringConfig = {
+    tableAlias: 'ic',
+    groupFieldAlias: 'activity',
+    lengthFieldAlias: 'adj_length',
+  };
+
+  getTDistance = function(subLayerID, displayOp) {
+    var sqlString = sqlBuilder.getDistanceSQL(
+      subLayerID,
+      tDistanceStringConfig
+    );
+    //console.log("HI")
+    console.log(sqlString);
+    sql.execute(sqlString).done(function(data) {
+      opsDisplay[displayOp](subLayerID, data);
+    });
+  };
+
+  getSegmentPosition = function(seg_id) {
+    var sqlString = sqlBuilder.getSegmentPositionSQL(seg_id);
+    console.log(sqlString);
+    return sql.execute(sqlString);
+  };
+
+  // Public API
+  return {
+    getSegmentPosition: function(seg_id) {
+      return getSegmentPosition(seg_id);
+    },
+    progress: function(subLayerID) {
+      return getTDistance(subLayerID, 'progress');
+    },
+    calcTDistance: function(subLayerID) {
+      return getTDistance(subLayerID, 'calcTDistance');
+    },
+
+    typeBreakdown: function(subLayerID) {
+      return getTDistance(subLayerID, 'typeBreakdown');
+    },
+
+    totalMiles: function(subLayerID) {
+      return getTDistance(subLayerID, 'totalMiles');
+    },
+
+    ociBreakdown: function(subLayerID) {
+      var sqlString = sqlBuilder.getDistanceSQL(subLayerID, {
+        tableAlias: subLayerID.replace('-', ''),
+        groupFieldAlias: 'color',
+        lengthFieldAlias: 'area',
+        order: 'ASC',
+      });
+
+      sql.execute(sqlString).done(function(data) {
+        opsDisplay.ociBreakdown(subLayerID, data);
+        opsDisplay.totalMiles(subLayerID, data);
+      });
+    },
+
+    ociAvg: function(subLayerID) {
+      var sqlString = sqlBuilder.getOCICalcSQL(subLayerID, 'avg');
+      sql.execute(sqlString).done(function(data) {
+        opsDisplay.ociAvg(subLayerID, data);
+      });
+    },
+
+    workByMonth: function(subLayerID) {
+      var sqlString = sqlBuilder.getDistanceSQL(subLayerID, {
+        tableAlias: 'ic',
+        groupFieldSQL:
+          'to_char(' + sqlBuilder.mapAlias('ic', 'work_end') + ", 'MM'), type",
+        lengthFieldAlias: 'adj_length',
+        order: 'ASC',
+      });
+
+      sql.execute(sqlString).done(function(data) {
+        opsDisplay.workByMonth(subLayerID, data);
+      });
+    },
+
+    bigNumbers: function(subLayerID, data) {
+      var sqlString = sqlBuilder.getDistanceSQL(subLayerID, {
+        tableAlias: 'ic',
+        groupFieldSQL:
+          'to_char(' + sqlBuilder.mapAlias('ic', 'work_end') + ", 'MM-YY')",
+        lengthFieldAlias: 'adj_length',
+        order: 'ASC',
+      });
+
+      sql.execute(sqlString).done(function(data) {
+        opsDisplay.totalMiles(subLayerID, data);
+        opsDisplay.avgMilesPerMonth(subLayerID, data);
+      });
+    },
+  };
+})();
